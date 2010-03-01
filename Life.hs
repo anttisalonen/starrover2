56864d7e182ef5209a2d67033b9a72752ef90c3f
Antti Salonen <ajsalonen@gmail.com>
2010-03-01 14:00:39 +0100

    added life functions

--- Life.hs
+++ Life.hs
@@ -2,30 +2,67 @@ module Life
 where
 
 import Data.List(foldl')
+import Text.Printf
 
+import Sector
+import StarSystem
 import Stars
 import Planets
+import Console
 
 data Country = Country {
-    getPop :: Int
+    getCountryName :: String
+  , getPop :: Int
   , getTL  :: Int
-  , getLoc :: String
+  , getLoc :: (Sector, StellarBody)
   }
 
-createLife :: [StellarBody] -> [Country]
-createLife ss = foldl' go [] ss
-  where go acc x = 
+instance Displayable Country where
+  display c = printf "%s - Population: %d - TL: %d - Location %s:\n%s"
+                (getCountryName c)
+                (getPop c)
+                (getTL c)
+                (show . fst $ getLoc c)
+                (display . snd $ getLoc c)
+
+createLife :: Sector -> [Country]
+createLife sec = foldl' go [] ss
+  where ss = concatMap getStars $ getGalaxySector sec
+        go acc x = 
           case getBodyType x of
             RockyPlanet ->
               if getMass x > 0.1 && 
                  minTemperature ss (getOrbit x) > 230 &&
                  maxTemperature ss (getOrbit x) < 360
-                 then newCountry "" : foldl' go acc (getSatellites x)
+                 then newCountry sec x : foldl' go acc (getSatellites x)
                  else foldl' go acc (getSatellites x)
             _ ->      foldl' go acc (getSatellites x)
 
-newCountry :: String -> Country
-newCountry l = Country 100 1 l
+newCountry :: Sector -> StellarBody -> Country
+newCountry sec s = Country (getName s) 100 1 (sec, s)
 
 stepDevelopment :: Country -> Country
-stepDevelopment = id
+stepDevelopment c = 
+  let mp = maxPop (snd . getLoc $ c)
+  in c{getPop = floor (fromIntegral (mp - (getPop c)) * 0.1) + (getPop c)}
+
+ratePlanet :: [StellarBody] -> StellarBody -> Float
+ratePlanet stars s | getBodyType s /= RockyPlanet = 0
+                   | otherwise                    =
+  let tempdiff = fromIntegral $ max (abs (maxTemperature stars (getOrbit s) - 300))
+                                    (abs (minTemperature stars (getOrbit s) - 300))
+      temppoints = max 0 (100 - tempdiff)
+      sizepoints = getMass s * 10
+  in min temppoints sizepoints
+
+rateSystem :: StarSystem -> [(String, Float)]
+rateSystem sys = 
+  filter (\(_, v) -> v > 0.0) $
+  zip (map getName (concatMap getSatellites (getStars sys)))
+      (map 
+         (\s -> ratePlanet (getStars sys) s)
+         (concatMap getSatellites (getStars sys)))
+
+rateSector :: Sector -> [(String, Float)]
+rateSector sec = concatMap rateSystem (getGalaxySector sec)
+
