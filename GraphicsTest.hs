84c08c0efcf42ac3092ef3490aae2db3e94b9ed7
Antti Salonen <ajsalonen@gmail.com>
2010-03-06 11:04:03 +0100

    added graphics test (originally from unknown in the web)

---
+++ GraphicsTest.hs
@@ -0,0 +1,130 @@
+module Main where
+
+import System.Exit
+import System.Environment
+import Control.Monad
+
+import Graphics.Rendering.OpenGL as OpenGL
+import Graphics.UI.SDL as SDL
+
+step = 0.5
+
+main = withInit [InitVideo] $ do
+  progName <- getProgName
+  blendEquation $= FuncAdd
+  blendFunc $= (OpenGL.SrcAlpha, OneMinusSrcAlpha)
+  createAWindow progName
+
+createAWindow name
+    = do setVideoMode width height 0 [OpenGL]
+         depthFunc $= Just Less
+         clearColor $= Color4 1 1 1 1
+         viewport $= (Position 0 0,Size (fromIntegral width) (fromIntegral height))
+         matrixMode $= Projection
+         loadIdentity
+         perspective 45 (w/h) 0.1 100
+         matrixMode $= Modelview 0
+         let loop rtri rquad stopped = do 
+                       delay 10
+                       (rtri', rquad') <- drawGLScreen rtri rquad stopped
+                       event <- pollEvent
+                       quit <- case event of
+                                 Quit -> return True
+                                 KeyDown (Keysym SDLK_q _ _) -> return True
+                                 _    -> return False
+                       let stopped' = case event of
+                                 KeyDown (Keysym SDLK_SPACE _ _) -> True
+                                 KeyUp (Keysym SDLK_SPACE _ _)   -> False
+--                                  VideoResize w h -> resizeGLScene w h >> return False
+                                 _ -> stopped
+                       when (not quit) (loop rtri' rquad' stopped')
+         loop 0 0 False
+      where width = 800
+            height = 600
+            (w,h) = (fromIntegral width, fromIntegral height)
+
+resizeGLScene w h = do
+  setVideoMode w h 0 [OpenGL,Resizable]
+  viewport $= (Position 0 0,Size (fromIntegral w) (fromIntegral h))
+  matrixMode $= Projection
+  loadIdentity
+  let radio = (fromIntegral w)/(fromIntegral h)
+  perspective 45 radio 0.1 100
+  matrixMode $= Modelview 0
+
+drawGLScreen :: GLfloat -> GLfloat -> Bool -> IO (GLfloat, GLfloat)
+drawGLScreen rtri rquad stopped = do
+  clear [ColorBuffer,DepthBuffer]
+  loadIdentity
+
+  translate $ (\(x,y,z) -> Vector3 x y z) polygonTrans
+  rotate rtri $ Vector3 0 (1::GLfloat) 0
+  rotate rtri $ Vector3 (1::GLfloat) 0 0
+  let rtri' = if stopped
+                then rtri
+                else rtri + step
+  mapM_ (\l -> do
+    renderPrimitive Polygon $ mapM_ (\((r,g,b),(x,y,z)) -> do
+      currentColor $= Color4 r g b 1
+      vertex$Vertex3 x y z) l
+    ) polygonPoints
+  
+  loadIdentity
+  translate $ (\(x,y,z) -> Vector3 x y z) quadsTrans
+  rotate rquad $ Vector3 (1::GLfloat) 0 0
+  rotate rquad $ Vector3 0 (1::GLfloat) 0
+  let rquad' = if stopped
+                 then rquad
+                 else rquad - step
+  currentColor $= Color4 0.5 0.5 1 0.5
+  renderPrimitive Quads $ mapM_ (\((r,g,b),l) -> do
+    currentColor $= Color4 r g b 0.5
+    mapM_ (\(x,y,z) -> vertex$Vertex3 x y z) l
+    ) quadsPoints
+  
+  glSwapBuffers
+  return (rtri', rquad')
+
+polygonTrans :: (GLfloat,GLfloat,GLfloat)
+polygonTrans = (-1.5,0,-9)
+
+polygonPoints :: [[((GLfloat,GLfloat,GLfloat),(GLfloat,GLfloat,GLfloat))]]
+polygonPoints =
+  [ [((1,0,0),(0,1,0))
+    ,((0,1,0),(-1,-1,1))
+    ,((0,0,1),(1,-1,1))] -- Front
+  , [((1,0,0),(0,1,0))
+    ,((0,0,1),(1,-1,1))
+    ,((0,1,0),(1,-1,-1))] -- Right
+  , [((1,0,0),(0,1,0))
+    ,((0,1,0),(1,-1,-1))
+    ,((0,0,1),(-1,-1,-1))] -- Back
+  , [((1,0,0),(0,1,0))
+    ,((0,0,1),(-1,-1,-1))
+    ,((0,1,0),(-1,-1,1))] -- Left
+  , [((0,0.5,0.5),(0,-1,0))
+    ,((0,0,1),(-1,-1,-1))
+    ,((0,1,0),(1,-1,-1))] -- Bottom Back
+  , [((0,0.5,0.5),(0,-1,0))
+    ,((0,0,1),(1,-1,1))
+    ,((0,1,0),(1,-1,-1))] -- Bottom Right
+  , [((0,0.5,0.5),(0,-1,0))
+    ,((0,0,1),(-1,-1,-1))
+    ,((0,1,0),(-1,-1,1))] -- Bottom Left
+  , [((0,0.5,0.5),(0,-1,0))
+    ,((0,1,0),(-1,-1,1))
+    ,((0,0,1),(1,-1,1))] -- Bottom Front
+  ]
+
+quadsTrans :: (GLfloat,GLfloat,GLfloat)
+quadsTrans = (1.5,0,-6)
+
+quadsPoints :: [((GLfloat,GLfloat,GLfloat),[(GLfloat,GLfloat,GLfloat)])]
+quadsPoints =
+  [((0,1,0),  [(1,1,-1) ,(-1,1,-1) ,(-1,1,1)  ,(1,1,1)  ]) -- Top
+  ,((1,0.5,0),[(1,-1,1) ,(-1,-1,1) ,(-1,-1,-1),(1,-1,-1)]) -- Bottom
+  ,((1,0,0),  [(1,1,1)  ,(-1,1,1)  ,(-1,-1,1) ,(1,-1,1) ]) -- Front
+  ,((1,1,0),  [(1,-1,-1),(-1,-1,-1),(-1,1,-1) ,(1,1,-1) ]) -- Back
+  ,((0,0,1),  [(-1,1,1) ,(-1,1,-1) ,(-1,-1,-1),(-1,-1,1)]) -- Left
+  ,((0,0,1),  [(1,1,-1) ,(1,1,1)   ,(1,-1,1)  ,(1,-1,-1)]) -- Right
+  ]
