daab33bb51898e67d161fbdd3e5f3891c94e3ef3
Antti Salonen <ajsalonen@gmail.com>
2010-03-06 15:41:01 +0100

    added statet monad

--- GraphicsTest.hs
+++ GraphicsTest.hs
@@ -4,14 +4,12 @@ module Main where
 import System.Exit
 import System.Environment
 import Control.Monad
+import Control.Monad.State as State
 
 import Graphics.Rendering.OpenGL as OpenGL
 import Graphics.UI.SDL as SDL
 
-step = (0.05, 0.0, 0.0)
-width = 800
-height = 600
-
+-- generic stuff
 (*+*) :: GLvector3 -> GLvector3 -> GLvector3
 (*+*) (x0, y0, z0) (x1, y1, z1) = (x0 + x1, y0 + y1, z0 + z1)
 
@@ -23,10 +21,24 @@ glVector3Null = (0, 0, 0)
 
 type GLvector3 = (GLfloat, GLfloat, GLfloat)
 
+-- test scenario
+step = (0.05, 0.0, 0.0)
+width = 800
+height = 600
+
+data TestState = TestState {
+    rtri    :: GLvector3
+  , rquad   :: GLvector3
+  , stopped :: Bool
+  }
+
 main = withInit [InitVideo] $ do
   blendEquation $= FuncAdd
   blendFunc $= (OpenGL.SrcAlpha, OneMinusSrcAlpha)
   createAWindow 
+
+initState :: TestState
+initState = TestState glVector3Null glVector3Null False
 
 createAWindow = do
   setVideoMode width height 0 [OpenGL]
@@ -37,18 +49,20 @@ createAWindow = do
   loadIdentity
   ortho ((-1) * width * 0.01) (1 * width * 0.01) ((-1) * height * 0.01) (1 * height * 0.01) (-10) 10
   matrixMode $= Modelview 0
-  loop glVector3Null glVector3Null False
+  evalStateT loop initState
 
-loop rtri rquad stopped = do 
-  delay 10
-  drawGLScreen rtri rquad
-  let rtri' = if stopped
-                then rtri
-                else rtri *+* step
-  let rquad' = if stopped
-                then rquad
-                else rquad *-* step
-  event <- pollEvent
+loop :: StateT TestState IO ()
+loop = do 
+  liftIO $ delay 10
+  state <- State.get
+  liftIO $ drawGLScreen (rtri state) (rquad state)
+  let rtri' = if (stopped state)
+                then (rtri state)
+                else (rtri state) *+* step
+  let rquad' = if (stopped state)
+                then (rquad state)
+                else (rquad state) *-* step
+  event <- liftIO $ pollEvent
   quit <- case event of
             Quit                        -> return True
             KeyDown (Keysym SDLK_q _ _) -> return True
@@ -56,8 +70,9 @@ loop rtri rquad stopped = do
   let stopped' = case event of
             KeyDown (Keysym SDLK_SPACE _ _) -> True
             KeyUp (Keysym SDLK_SPACE _ _)   -> False
-            _                               -> stopped
-  when (not quit) (loop rtri' rquad' stopped')
+            _                               -> (stopped state)
+  put (TestState rtri' rquad' stopped')
+  when (not quit) loop
 
 drawGLScreen :: GLvector3 -> GLvector3 -> IO ()
 drawGLScreen rtri rquad = do
