ca9ef207d82079b549e53e313e26a3df614b0aae
Antti Salonen <ajsalonen@gmail.com>
2010-03-06 15:01:09 +0100

    extract main graphics loop

--- GraphicsTest.hs
+++ GraphicsTest.hs
@@ -9,6 +9,8 @@ import Graphics.Rendering.OpenGL as OpenGL
 import Graphics.UI.SDL as SDL
 
 step = 0.5
+width = 800
+height = 600
 
 main = withInit [InitVideo] $ do
   progName <- getProgName
@@ -16,38 +18,36 @@ main = withInit [InitVideo] $ do
   blendFunc $= (OpenGL.SrcAlpha, OneMinusSrcAlpha)
   createAWindow progName
 
-createAWindow name
-    = do setVideoMode width height 0 [OpenGL]
-         depthFunc $= Just Less
-         clearColor $= Color4 1 1 1 1
-         viewport $= (Position 0 0, Size width height)
-         matrixMode $= Projection
-         loadIdentity
-         ortho ((-1) * width * 0.01) (1 * width * 0.01) ((-1) * height * 0.01) (1 * height * 0.01) (-10) 10
-         matrixMode $= Modelview 0
-         let loop rtri rquad stopped = do 
-                       delay 10
-                       drawGLScreen rtri rquad
-                       let rtri' = if stopped
-                                     then rtri
-                                     else rtri + step
-                       let rquad' = if stopped
-                                     then rquad
-                                     else rquad - step
-                       event <- pollEvent
-                       quit <- case event of
-                                 Quit -> return True
-                                 KeyDown (Keysym SDLK_q _ _) -> return True
-                                 _    -> return False
-                       let stopped' = case event of
-                                 KeyDown (Keysym SDLK_SPACE _ _) -> True
-                                 KeyUp (Keysym SDLK_SPACE _ _)   -> False
---                                  VideoResize w h -> resizeGLScene w h >> return False
-                                 _ -> stopped
-                       when (not quit) (loop rtri' rquad' stopped')
-         loop 0 0 False
-      where width = 800
-            height = 600
+createAWindow name = do
+  setVideoMode width height 0 [OpenGL]
+  depthFunc $= Just Less
+  clearColor $= Color4 1 1 1 1
+  viewport $= (Position 0 0, Size width height)
+  matrixMode $= Projection
+  loadIdentity
+  ortho ((-1) * width * 0.01) (1 * width * 0.01) ((-1) * height * 0.01) (1 * height * 0.01) (-10) 10
+  matrixMode $= Modelview 0
+  loop 0 0 False
+
+loop rtri rquad stopped = do 
+  delay 10
+  drawGLScreen rtri rquad
+  let rtri' = if stopped
+                then rtri
+                else rtri + step
+  let rquad' = if stopped
+                then rquad
+                else rquad - step
+  event <- pollEvent
+  quit <- case event of
+            Quit -> return True
+            KeyDown (Keysym SDLK_q _ _) -> return True
+            _    -> return False
+  let stopped' = case event of
+            KeyDown (Keysym SDLK_SPACE _ _) -> True
+            KeyUp (Keysym SDLK_SPACE _ _)   -> False
+            _ -> stopped
+  when (not quit) (loop rtri' rquad' stopped')
 
 resizeGLScene w h = do
   setVideoMode w h 0 [OpenGL,Resizable]
