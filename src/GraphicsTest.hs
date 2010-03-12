80c86bf3a601306faa8787a3bb8bb981b658d1c7
Antti Salonen <ajsalonen@gmail.com>
2010-03-12 20:21:23 +0100

    raise window size

--- src/GraphicsTest.hs
+++ src/GraphicsTest.hs
@@ -27,8 +27,8 @@ hasEvent :: (SDL.Event -> Bool) -> [SDL.Event] -> Bool
 hasEvent fun evts = or $ map fun evts
 
 -- test scenario
-width = 640
-height = 480
+width = 800
+height = 600
 
 data TestState = TestState {
     tri          :: Entity
