af0db2680b2d668169f7a0f297e7897dc00ff053
Antti Salonen <ajsalonen@gmail.com>
2010-03-14 21:15:27 +0100

    add trading

--- src/Cargo.hs
+++ src/Cargo.hs
@@ -3,6 +3,7 @@ where
 
 import System.Random
 import Text.Printf
+import Data.List
 import Control.Monad
 
 import qualified Data.Edison.Assoc.StandardMap as M
@@ -10,21 +11,21 @@ import qualified Data.Edison.Assoc.StandardMap as M
 import Statistics
 
 type Cargo = M.FM String Int
-type Market = [(String, (Int, Int))]
+type Market = M.FM String (Int, Int)
 
-cargonames = ["Grain", "Fruit", "Gem stones", "Firearms"]
+cargonames = sort ["Grain", "Fruit", "Gem stones", "Firearms"]
 
 randomCargo :: IO String
 randomCargo = chooseIO cargonames
 
 showCargo :: Cargo -> String
 showCargo c | M.null c  = "No cargo\n"
-            | otherwise = concatMap (\(k, v) -> printf "%-20s-%4d\n" k v) (M.toSeq c) 
+            | otherwise = concatMap (\(k, v) -> printf "%-20s-%4d\n" k v) (M.toOrdSeq c) 
 
 showMarket :: Market -> String
 showMarket m = title ++ infos
   where title = printf "%-16s%8s%8s\n" "Good" "Quantity" "Price"
-        infos = concatMap (\(n, (q, p)) -> printf "%-20s%10d%10d\n" n q p) m
+        infos = concatMap (\(n, (q, p)) -> printf "%-20s%10d%10d\n" n q p) (M.toOrdSeq m)
 
 numCargoItems = length cargonames
 
@@ -33,7 +34,7 @@ randomMarket = do
   let names = cargonames
   prices <- replicateM numCargoItems $ randomRIO (5, 15)
   quantities <- replicateM numCargoItems $ randomRIO (0, 30)
-  return $ zip names (zip quantities prices)
+  return $ M.fromSeq $ zip names (zip quantities prices)
 
 fitCargo :: Cargo -> [(String, Int)]
 fitCargo c = map (\s -> (s, M.lookupWithDefault 0 s c)) cargonames
@@ -41,4 +42,4 @@ fitCargo c = map (\s -> (s, M.lookupWithDefault 0 s c)) cargonames
 showMarketAndCargo :: Market -> Cargo -> String
 showMarketAndCargo m c = title ++ infos
   where title = printf "%-14s%9s%6s%6s\n" "Good" "Quantity" "Price" "Cargo"
-        infos = concatMap (\((n, (q, p)), (_, q')) -> printf "%-14s%9d%6d%6d\n" n q p q') (zip m (fitCargo c))
+        infos = concatMap (\((n, (q, p)), (_, q')) -> printf "%-14s%9d%6d%6d\n" n q p q') (zip (M.toOrdSeq m) (fitCargo c))
--- src/SpaceState.hs
+++ src/SpaceState.hs
@@ -32,6 +32,8 @@ data TestState = TestState {
   , gamefont     :: Font
   , monofont     :: Font
   , cargo        :: Cargo
+  , cash         :: Int
+  , lastmarket   :: (String, Market)
   }
 
 -- TODO: generate mod-functions using TH
@@ -49,6 +51,12 @@ modStopped f t = t{stopped = f (stopped t)}
 
 modCargo :: (Cargo -> Cargo) -> TestState -> TestState
 modCargo f t = t{cargo = f (cargo t)}
+
+modCash :: (Int -> Int) -> TestState -> TestState
+modCash f t = t{cash = f (cash t)}
+
+modMarket :: ((String, Market) -> (String, Market)) -> TestState -> TestState
+modMarket f t = t{lastmarket = f (lastmarket t)}
 
 aobjs =
   [ AObject "Star"       0   (Color4 0.9 0.0 0.0 1.0) 6.0 0
@@ -73,6 +81,8 @@ initState f f2 = TestState
     f
     f2
     M.empty
+    100
+    ("", M.empty)
 
 zoomChangeFactor :: (Floating a) => a
 zoomChangeFactor = 1.0
@@ -123,24 +133,81 @@ handleEvents = do
   processEvents inputMapping events
   return $ isQuit events
 
-gotoCity :: String -> StateT TestState IO ()
-gotoCity n = do
+buy :: Int -> String -> StateT TestState IO ()
+buy q n = do
   state <- State.get
-  market <- liftIO $ randomMarket
+  let mval = M.lookupM n (snd (lastmarket state))
+  case mval of
+    Nothing      -> return ()
+    Just (q', p) -> do
+      let totalq = min (max 0 q') q
+      let totalp = totalq * p
+      if totalp > cash state || totalq == 0
+        then return ()
+        else do
+          modify $ modMarket $ modSnd $ fromMarket totalq n
+          modify $ modCargo $ toCargo totalq n
+          modify $ modCash $ (subtract totalp)
+
+sell :: Int -> String -> StateT TestState IO ()
+sell q n = do
+  state <- State.get
+  let mval = M.lookupM n (cargo state)
+  case mval of
+    Nothing -> return ()
+    Just q' -> buy (negate (min q' q)) n
+
+fromMarket :: Int -> String -> Market -> Market
+fromMarket q = toMarket (-q)
+
+fromCargo :: Int -> String -> Cargo -> Cargo
+fromCargo q = toCargo (-q)
+
+toMarket :: Int -> String -> Market -> Market
+toMarket q = M.adjust (\(q', p) -> (q' + q, p))
+
+toCargo :: Int -> String -> Cargo -> Cargo
+toCargo q n = M.insertWith (+) n q
+
+modFst :: (a -> a) -> (a, b) -> (a, b)
+modFst f (a, b) = (f a, b)
+
+modSnd :: (b -> b) -> (a, b) -> (a, b)
+modSnd f (a, b) = (a, f b)
+
+gotoCity :: String -> StateT TestState IO ()
+gotoCity planetname = do
+  state <- State.get
+  nmarket <- if planetname == fst (lastmarket state)
+               then return $ lastmarket state
+               else do
+                 m <- liftIO $ randomMarket
+                 return (planetname, m)
+  let market = snd nmarket
+  modify $ modMarket $ const nmarket
   let exitb = ((100, 100), (100, 30)) :: (Num a) => ((a, a), (a, a))
       buybuttons  = map (\i -> ((550, 440 - 50 * fromIntegral i), (100, 30))) [1..numCargoItems] :: (Num a) => [((a, a), (a, a))]
       sellbuttons = map (\i -> ((680, 440 - 50 * fromIntegral i), (100, 30))) [1..numCargoItems] :: (Num a) => [((a, a), (a, a))]
+      buyactions  = map (\(n, (_, _)) -> buy  1 n >> return Nothing) (M.toOrdSeq market)
+      sellactions = map (\(n, (_, _)) -> sell 1 n >> return Nothing) (M.toOrdSeq market)
+      allbuttons  = exitb : (buybuttons ++ sellbuttons)
+      allactions  = return (Just ()) : (buyactions ++ sellactions)
+      bttoaction  = zip allbuttons allactions
   let handleInput = do
         events <- liftIO $ pollAllSDLEvents
-        if mouseClickIn [ButtonLeft] exitb events
-          then return $ Just ()
-          else return $ Nothing
-  loopTextScreen (liftIO $ makeTextScreen (10, 500) 
-                               [(gamefont state, Color4 1.0 1.0 1.0 1.0, "Landed on " ++ n),
-                                (monofont state, Color4 1.0 1.0 0.0 1.0, showMarketAndCargo market (cargo state))]
-                               (drawButton "Exit" (gamefont state) exitb >>
-                                mapM_ (drawButton "Buy" (gamefont state)) buybuttons >>
-                                mapM_ (drawButton "Sell" (gamefont state)) sellbuttons))
+        let mbutton = mouseClickInAny [ButtonLeft] allbuttons events
+        case mbutton of
+          Nothing -> return Nothing
+          Just n  -> case lookup n bttoaction of
+                       Just act -> act
+                       Nothing  -> return Nothing
+  loopTextScreen (do st <- State.get; liftIO $ makeTextScreen (10, 500) 
+                               [(gamefont st, Color4 1.0 1.0 1.0 1.0, "Landed on " ++ planetname),
+                                (monofont st, Color4 1.0 1.0 0.0 1.0, showMarketAndCargo (snd (lastmarket st)) (cargo st)),
+                                (gamefont st, Color4 1.0 1.0 1.0 1.0, "Cash: " ++ show (cash st))]
+                               (drawButton "Exit" (gamefont st) exitb >>
+                                mapM_ (drawButton "Buy" (gamefont st)) buybuttons >>
+                                mapM_ (drawButton "Sell" (gamefont st)) sellbuttons))
                  handleInput
   return ()
 
