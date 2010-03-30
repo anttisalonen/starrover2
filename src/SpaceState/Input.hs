274b6c7940b96e1b995f3ddfcd13b90adf5cd401
Antti Salonen <ajsalonen@gmail.com>
2010-03-30 22:07:04 +0200

    add function for displaying star system map

--- src/SpaceState/Input.hs
+++ src/SpaceState/Input.hs
@@ -4,10 +4,13 @@ where
 import Data.List
 import Control.Monad
 import Control.Monad.State as State
-import Prelude hiding (catch)
+import Prelude hiding (catch, until)
 
 import Graphics.UI.SDL as SDL
 
+import Utils
+import Space
+import OpenGLUtils
 import Entity
 import Camera
 import AObject
@@ -32,7 +35,18 @@ inputMapping =
   , (SDLK_PLUS,  (changeZoom (-zoomChangeFactor), setZoomDelta 0))
   , (SDLK_i,     (showInfo, return ()))
   , (SDLK_p,     (modify $ modStopped not, return ()))
+  , (SDLK_m,     (showMap, return ()))
   ]
+
+showMap :: StateT SpaceState IO ()
+showMap = do
+  state <- State.get
+  let maxrad = maximum $ map orbitRadius $ aobjects state
+  let ((minx', maxx'), (miny', maxy')) = boxThatIncludes (-maxrad, maxrad) (-maxrad, maxrad) 10 10 width height
+  liftIO $ until anyKeyOrMouseWasPressedIO $ 
+    inOrthoBoxDraw minx' maxx' miny' maxy' (-10) 10 $ do
+      delay 10
+      drawGLScreen [tri state] (aobjects state)
 
 showInfo = do
   s <- State.get
